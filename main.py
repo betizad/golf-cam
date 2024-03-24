505db9b1eb38e860da07fc72fa2821892f35c43f
Behzad Bayat <betizad@users.noreply.github.com>
2024-03-24 16:29:02 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from streamlit_image_select import image_select
+
+
+st.set_page_config(layout="wide")
+
+img = image_select(
+    label="Select a webcam",
+    images=[
+        "https://www.golfparks.ch/Frontend/Sublayouts/Modules/ApplicationModules/Weather/Data/Web-Cams/Otelfingen/image-OF00001.jpg",
+        "https://www.golfparks.ch/Frontend/Sublayouts/Modules/ApplicationModules/Weather/Data/Web-Cams/Otelfingen/image-OF00002.jpg",
+        "https://www.golfparks.ch/Frontend/Sublayouts/Modules/ApplicationModules/Weather/Data/Web-Cams/Otelfingen/image-OF00003.jpg",
+        "https://www.golfparks.ch/Frontend/Sublayouts/Modules/ApplicationModules/Weather/Data/Web-Cams/Otelfingen/image-OF00004.jpg",
+        "https://www.golfparks.ch/Frontend/Sublayouts/Modules/ApplicationModules/Weather/Data/Web-Cams/Otelfingen/image-OF00005.jpg",
+    ],
+    captions=["Cam Range Mats", "Cam2", "Cam3", "Cam Putting", "Cam5"],
+    use_container_width=False,
+    
+)
+# print(img)
+st.image(img)

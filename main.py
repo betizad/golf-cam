6e12eb76c7faf69219df4f34508d695ff5f79d26
Behzad Bayat <betizad@users.noreply.github.com>
2024-03-30 21:48:50 +0000

    add Holzhäusern

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
 import streamlit as st
 from streamlit_image_select import image_select
 
-st.set_page_config(layout="wide", page_title="Otelfingen Webcams")
-st.markdown("### Otelfingen Webcams")
+st.set_page_config(layout="wide", page_title="Golf Webcams")
 
-img = image_select(
-    label="Select a webcam",
+images=[]
+captions=[]
+
+location = st.sidebar.selectbox(
+                              'Choose Location:',
+                              ('Otelfingen', 'Holzhäusern')
+                            )
+page_title=f"{location} Webcams"
+
+if location=="Otelfingen":
     images=[
         "https://www.golfparks.ch/Frontend/Sublayouts/Modules/ApplicationModules/Weather/Data/Web-Cams/Otelfingen/image-OF00001.jpg",
         "https://www.golfparks.ch/Frontend/Sublayouts/Modules/ApplicationModules/Weather/Data/Web-Cams/Otelfingen/image-OF00002.jpg",
         "https://www.golfparks.ch/Frontend/Sublayouts/Modules/ApplicationModules/Weather/Data/Web-Cams/Otelfingen/image-OF00003.jpg",
         "https://www.golfparks.ch/Frontend/Sublayouts/Modules/ApplicationModules/Weather/Data/Web-Cams/Otelfingen/image-OF00004.jpg",
         "https://www.golfparks.ch/Frontend/Sublayouts/Modules/ApplicationModules/Weather/Data/Web-Cams/Otelfingen/image-OF00005.jpg",
-    ],
-    captions=["Cam Range Mats", "Cam2", "Cam3", "Cam Putting", "Cam5"],
-    use_container_width=False,
+    ]
+    captions=["Cam Range Mats", "Cam2", "Cam3", "Cam Putting", "Cam5"]
+elif location=="Holzhäusern":
+    images=[
+        "https://www.golfparks.ch/Frontend/Sublayouts/Modules/ApplicationModules/Weather/Data/Web-Cams/Holzhaeusern/image01.jpg",
+        "https://www.golfparks.ch/Frontend/Sublayouts/Modules/ApplicationModules/Weather/Data/Web-Cams/Holzhaeusern/image02.jpg"
+    ]
+    captions=["Range", "Putting"]
+else:
+    pass
     
+st.markdown(f"### {page_title}")
+img = image_select(
+    label="Select a webcam",
+    images=images,
+    captions=captions,
+    use_container_width=False
 )
-# print(img)
 st.image(img)
